c3a562da5b227d438037aeeb5b318e1a8d90cf0b
JBEDU000 <JBEDU000@JBEDU00>
2022-04-04 16:05:29 +0900

    jdbc Test

--- src/jdbcTest/JDBC_EX01.java
+++ src/jdbcTest/JDBC_EX01.java
@@ -7,11 +7,39 @@ public class JDBC_EX01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String driverName = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/student_db";
+		String username = "root";
+		String password = "12345";
+		String query = "select * from smembers";
+		String query2 = "delete from smembers where id='tiger'";
+		
+		ResultSet ret = null;
+		Connection conn = null;//DB 연결 객체 선언
+		Statement stmt = null;//sql 실행 객체 선언
+		
+		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");//mysql jdbc 드라이버 호출
-			Connection conn = 
-					DriverManager.getConnection("jdbc:mysql://localhost:3306/student_db","root","12345");
+			Class.forName(driverName);//mysql jdbc 드라이버 호출
+			conn = DriverManager.getConnection(url,username,password);
 			System.out.println("DB 연결 완료!!!");			
+			stmt = conn.createStatement();//SQL실행용 statment 객체 생성
+			ret = stmt.executeQuery(query);//SQL실행-반환되는 결과값을 ResultSet으로 받음
+			
+			while(ret.next()) {
+				String pid = ret.getString("id");
+				String ppw = ret.getString("pw");
+				System.out.println(pid);
+				System.out.println(ppw);				
+			}
+			
+			int rnum = stmt.executeUpdate(query2);//성공여부 반환 int 1이면 성공
+			
+			if (rnum == 1) {
+				System.out.println("회원탈퇴성공!!!");
+			} else {
+				System.out.println("회원삭제실패!!!");
+			}
 			
 		} catch(ClassNotFoundException e) {
 			System.out.println("JDBC 드라이버 로드 에러!!!");
